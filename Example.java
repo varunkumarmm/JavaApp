5d146aa6b62130e41f3136b983d59719f134db05
varunkumarmm <97875349+varunkumarmm@users.noreply.github.com>
2022-01-17 17:52:39 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,5 +1,7 @@
-class Example {
-public static void main(String []args) {
-	System.out.println("Hi");
-}
+class Example 
+{
+	public static void main(String []args)
+	{
+		System.out.println("Hi");
+	}
 }
